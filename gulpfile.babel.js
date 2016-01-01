7255150463dffe26d10969ef2c6860f2440e2afe
1000ch <shogo.sensui@gmail.com>
2016-01-01 23:02:46 +0900

    fix padding & css integration

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,27 +5,33 @@ const autoprefixer = require('gulp-autoprefixer');
 const csscomb = require('gulp-csscomb');
 const csso = require('gulp-csso');
 
+const JS_APP_FILES = [
+  'static/js/app.js'
+];
+
+const CSS_APP_FILES = [
+  'static/css/app.css',
+  'static/css/settings.css',
+  'static/css/tools.css',
+  'static/css/generic.css',
+  'static/css/base.css',
+  'static/css/objects.css',
+  'static/css/components.css',
+  'static/css/trumps.css'
+];
+
 gulp.task('build', () => gulp.start('js:app', 'css:app'));
 
 gulp.task('js:app', () => {
-  return gulp.src([
-    'static/js/app.js'
-  ]).pipe(concat('app.min.js'))
+  return gulp.src(JS_APP_FILES)
+    .pipe(concat('app.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('public/js'));
 });
 
 gulp.task('css:app', () => {
-  return gulp.src([
-    'static/css/app.css',
-    'static/css/settings.css',
-    'static/css/tools.css',
-    'static/css/generic.css',
-    'static/css/base.css',
-    'static/css/objects.css',
-    'static/css/components.css',
-    'static/css/trumps.css'
-  ]).pipe(concat('app.min.css'))
+  return gulp.src(CSS_APP_FILES)
+    .pipe(concat('app.min.css'))
     .pipe(autoprefixer())
     .pipe(csscomb())
     .pipe(csso())
