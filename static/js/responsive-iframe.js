bb3f9f72c265f190e0828bce4d23c399d4827ff9
1000ch <shogo.sensui@gmail.com>
2020-01-01 18:40:02 +0900

    Add <responsive-iframe> component

---
+++ static/js/responsive-iframe.js
@@ -0,0 +1,43 @@
+import {
+  LitElement,
+  html,
+  css
+} from 'https://unpkg.com/lit-element/lit-element.js?module';
+
+export default class ResponsiveIframe extends LitElement {
+  static get properties() {
+    return {
+      src: String
+    };
+  }
+
+  static get styles() {
+    return css`
+      .ResponsiveIframe {
+        position: relative;
+        width: 100%;
+        padding-top: 56.25%;
+      }
+
+      .ResponsiveIframe iframe {
+        position: absolute;
+        top: 0;
+        right: 0;
+        width: 100% !important;
+        height: 100% !important;
+      }
+`;
+  }
+
+  render() {
+    return html`
+      <div class="ResponsiveIframe">
+        <iframe
+          src="${this.src}"
+          frameborder="0"
+          allowfullscreen>
+        </iframe>
+      </div>
+    `;
+  }
+}
